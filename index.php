e57808c7f722540152fb2a6ea68f5d20981ddda4
samiraola <samiola0102@gmail.com>
2023-10-12 10:18:57 +0000

    third

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <title>La premiere page</title>
     <link rel="stylesheet" href="./css/index.css">
 <style>
+        body{
+            background-color:chocolate;
+        }
         .deuxieme{
             width: 100%;
             height: 200px;
@@ -35,8 +38,10 @@
            display: flex;
           align-items: center;
           flex-direction: column;
-          
-
+          align-items:center;
+        }
+        .deuxieme .recherche{
+            margin-top: 70px;
         }
         .recherche input{
             background-color: #ffffff2e;
@@ -56,6 +61,142 @@
             letter-spacing: 1px;
             text-indent: 35px;
             border: none;
+        }
+        .boutton button{
+            margin: 10px;
+            padding: 10px 15px;
+            outline: none;
+            border-radius: 60px;
+            font-size: 16px;
+            background-color: #FF9504;
+            font-size: 17px;
+            text-transform: uppercase;
+            cursor: pointer;
+            border: none;
+        }
+        .contactez-nous{
+            width:100%;
+           height:620px;
+            
+            border: 1px solid #72582c;         
+        }
+        .contactez-nous h3{
+            text-align:center;
+        }
+        form{
+            display:flex;
+            justify-content:space-around;
+        }
+        form .un{
+           display:flex;
+           
+        }
+        
+        form .trois{
+            text-align:justify;
+            width : 50%;
+            height:200px;
+           margin-top:30px;
+           
+        }
+       
+        form .trois p{
+            width:300px;
+            margin-top:30px;
+        }
+        form .trois input{
+            min-height: 30px;
+            padding: 5px 10px;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            
+        }
+        .un .trois{
+            margin:50px;
+            padding:10px;
+        }
+        form .trois label {
+            display:block;
+        }
+        .trois button{
+            display:block;
+            margin: 10px;
+            padding: 10px 15px;
+            outline: none;
+            border-radius: 60px;
+            font-size: 16px;
+            background-color: #FF9504;
+            font-size: 17px;
+            text-transform: uppercase;
+            cursor: pointer;
+            border: none;
+        }
+        form .deux{
+            display:flex;
+            flex-direction:column;
+            justify-content:right;
+            text-align:justify;
+           width : 50%;
+          margin-top:50px;
+        }
+        .un .deux{
+            margin:50px;
+            padding:10px;
+        }
+        .un .deux span{
+            width:300px;
+            margin-top:30px;
+        }
+        .photo{
+            width: 400px;
+            height: 200px;
+            background-image:url(images/mishaalzahed-h4x-Tksufvw-unsplash.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            margin-left:auto;
+        }
+        .photo p{
+            
+            text-align:center;
+            color:white;
+        }
+        .photo form{
+            display:block;
+            align-items:center;
+            justify-content:center;
+            margin-top:40px;
+        }
+        .photo form input{
+           
+            min-height: 30px;
+            padding: 5px 10px;
+            outline: none;
+            border: none;
+        }
+        .photo form button{
+           
+            margin: 10px;
+            padding: 10px 15px;
+            outline: none;
+            border-radius: 20px;
+            font-size: 16px;
+            background-color: #FF9504;
+            font-size: 17px;
+            text-transform: uppercase;
+            cursor: pointer;
+            border: none;
+        }
+        .trouver{
+            width: 400px;
+            height: 200px;
+            background-color:grey;
+        }
+        .footer{
+            width : 100%;
+            height: 50px;
+            background-color:grey;
         }
 
 </style>
@@ -136,6 +277,62 @@
     <div class="image"></div>
     <div class="recherche">
         <p>Je recherche un ouvrage , un auteur , un sujet</p><input type="text" name="" id="" placeholder="chercher"></div>
-</main>    
+</main> 
+
+<section class="bouton">
+    <div class="boutton">
+        <button type="button">Voir nos catalogues</button>
+    </div>
+</section>
+
+<main class="contactez-nous">
+    <h3>contactez-nous</h3>
+    <form action="" method="post">
+            <div class="un">
+                <div class="trois">
+                    <span>Envoyez-nous un message</span>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum modi quod doloremque molestias ipsum quidem enim tenetur eveniet. Quidem, neque ut aliquid amet reiciendis illum vero ipsum consequatur beatae, perferendis tempora delectus aut iusto quae inventore suscipit corrupti qui exercitationem molestiae! Neque nihil ullam, obcaecati e.</p>
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom" id="nom">
+                    <label for="email">email</label>
+                    <input type="text" name="email" id="email">
+                    <label for="">Votre question</label>
+                    <input type="text" name="question" id="question">
+                    <button type="button">Envoyez</button>
+                </div>
+               
+           
+                    <div class="deux">
+                        <p>trouvez-nous</p>
+                        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, repudiandae repellat deserunt iusto necessitatibus mollitia officiis asperiores consectetur accusantium ducimus?</span>
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quibusdam?</span>
+                        <span>Lorem ipsum dolor sit amet.</span>
+                    </div>
+
+                    
+            </div>
+    
+        </form>
+        <div class="photo">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, pariatur.</p>
+        <form action="" method="post">
+            <input type="text" name="" id="">
+            <button type="button">Abonnez-vous</button>
+        </form>
+    </div>
+</main>
+
+<section class="nous-trouver">
+    <h3>Nous trouver</h3>
+   <div class="trouver">
+
+   </div>
+</section>
+<section class="footer">
+    <div class="foot">
+
+    </div>
+</section>
+
 </body>
 </html>
--- voir.php
+++ voir.php
@@ -4,60 +4,122 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="./css/connexion.css">
+    <link rel="stylesheet" href="">
     <style>
-        section {
-            flex-grow: 1;
+        * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+body {
+    width: 100%;
+    min-height: 100vh;
+    
+   
+}
+
+nav {
+    display: flex;
+    padding: 10px 30px;
+    background-color: burlywood;
+    justify-content: space-evenly;
+    gap: 2em;
+    align-items: center;
+    color: #fff
+}
+
+nav a {
+    text-decoration: none;
+    color: #fff
+}
+
+nav .logo {
+    padding: 10px;
+    border: 1px solid #fff;
+    font-size: 1.8em;
+}
+
+nav .logo:hover {
+    background-color: #fff;
+    color: burlywood;
+    transition: all .2s ease-in-out;
+}
+
+nav ul {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 1em;
+    list-style: none;
+    align-items: center;
+    flex-grow: 1;
+}
+
+nav ul li {
+    min-width: 100px;
+    padding: 5px;
+}
+
+nav ul li a {
+    font-size: 1.1em;
+}
+
+nav ul .inscription {
+    color: black;
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+}
+
+nav ul .inscription:hover {
+    padding: 12px;
+    transition: all .2s ease-in-out;
+}
+
+nav ul input {
+    min-height: 30px;
+    padding: 5px 10px;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+   
+}
+
+@media screen and (max-width: 930px) {
+   nav {
+        flex-direction: column;
+    }
+}
+
+@media screen and (max-width: 730px) {
+    ul {
+        flex-direction: column;
+    }
+
+   nav ul form,
+    input {
+        width: 100%;
+    }
+}
+section {
             padding: 20px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
+            display:flex;
+            justify-content:space-evenly;
         }
-
-        #content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 2em;
-            width: 100%;
-            max-width: 1200px;
+        #content{
+            width:500px;
+            height:500px;
+            display:flex;
+            justify-content:left;
+            align-items:left;
+            
+            margin-right: auto;
+            background-color:red;
         }
-
-        #content .livre{
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            max-width: 900px;
-            gap: 1.5em;
-        }
-
-        #content .livre .title{
-            text-transform: uppercase;
-            text-align: center;
-        }
-
         #content .livre img{
-            width: 100%;
-           
-        }
-
-        #content .livre p{
-            text-align: justify;
-        }
-
-        #content .livre .description{
-            padding: 10px;
-            font-size: 1.2em;
-            font-weight: 900;
-            color: #333;
-        }
-
-        #content .livre .details{
-            width: 100%;
-            display: flex;
-            padding: 5px;
-            justify-content: space-between;
-            font-weight: 700;
+            width:50%;
+            float:left; 
         }
 
     </style>
@@ -81,7 +143,7 @@
         <div id="content">
             <div class="livre">
                 <h3 class="title">Mon Article 1</h3>
-                <img src="images/photo_2023-10-04_16-46-34.jpg" alt="">
+                <img src="images/photo_2023-10-04_16-46-34.jpg"  class="image" alt="">
                 <div class="description">
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, libero? Ipsa magnam, eaque
                         consectetur fuga esse accusamus voluptatem, laudantium quidem nulla nihil maiores iure
